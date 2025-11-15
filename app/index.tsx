67bb5444ff6741557a923913c9ddc1ea8d6ce2d1
Fachri Hawari <fachri.hawari@gmail.com>
2025-11-15 17:09:10 +0700

    chore: update package.json to include main entry and add new dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
-import "./app.css"
 import { Text, View } from 'react-native';
 
-export default function App() {
+export default function IndexPage() {
   return (
     <View className='flex-1 bg-white items-center justify-center'>
       <Text className="text-4xl">Open up App.tsx to start working on your app!</Text>
