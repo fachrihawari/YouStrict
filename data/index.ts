162379ff23b157efa58e58249850807a8d3f1be5
Fachri Hawari <fachri.hawari@gmail.com>
2025-12-04 21:56:04 +0700

    feat: Add new video entries for BabyBus, Tayo, and MeowMi Family Show

--- data/index.ts
+++ data/index.ts
@@ -12,6 +12,9 @@ import NussaOfficialSeriesEntries from './NussaOfficialSeries.json';
 import SiKecilPintar_Entries from './SiKecilPintar_.json';
 import cocobi_idEntries from './cocobi_id.json';
 import LittleHeroSuperZIndonesiaEntries from './LittleHeroSuperZIndonesia.json';
+import BabyBusIDEntries from './BabyBusID.json';
+import TayoBahasaIndonesiaEntries from './TayoBahasaIndonesia.json';
+import MeowMiFamilyShowIDEntries from './MeowMiFamilyShowID.json';
 
 export const entries = [
   ...cocobitoys_idEntries,
@@ -27,6 +30,9 @@ export const entries = [
   ...SiKecilPintar_Entries,
   ...cocobi_idEntries,
   ...LittleHeroSuperZIndonesiaEntries,
+  ...BabyBusIDEntries,
+  ...TayoBahasaIndonesiaEntries,
+  ...MeowMiFamilyShowIDEntries,
 ];
 
-export const lastUpdatedAt = '2025-12-04T14:48:09.876Z';
+export const lastUpdatedAt = '2025-12-04T14:55:53.898Z';
